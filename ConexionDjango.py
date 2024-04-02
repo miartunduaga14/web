3b2be8258eee088d73ae30334d64032c8f0e003c
miartunduaga14 <154467806+miartunduaga14@users.noreply.github.com>
2024-04-02 09:38:07 -0500

    Create ConexionDjango.py

---
+++ ConexionDjango.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myDatabase',
+        'USER': 'myDatabaseUser',
+        'PASSWORD': 'myPassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
